3ad5c471ff002ec6d1fb01d77a48a752af857a2d
Mike Allison <mike@mikeallisononline.com>
2015-11-02 14:27:33 -0600

    inital building commit

--- src/isparta.js
+++ src/isparta.js
@@ -1,3 +1,4 @@
 export * from 'istanbul';
 export {Instrumenter} from './instrumenter';
-export {VERSION} from '../package.json';
+let VERSION = require('../package.json').version;
+module.export = VERSION;
